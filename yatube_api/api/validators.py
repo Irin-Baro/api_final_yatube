2d2b93fac9914491854d57a20a089aa8af41582c
Ирина Баронская <barons42@yandex.ru>
2023-06-13 14:32:00 +0300

    Third commit

--- yatube_api/api/validators.py
+++ yatube_api/api/validators.py
@@ -2,17 +2,22 @@ from rest_framework.serializers import ValidationError
 
 
 class UniqueFieldsValueValidator:
-    requires_context = True
-
     def __init__(self, fields):
         self.fields = fields
 
-    def __call__(self, data, serializer):
-        field_values = []
-        for field in self.fields:
-            value = data[field]
-            if value in field_values:
-                raise ValidationError(f'Значение "{value}" '
-                                      f'поля "{field}" не уникально.')
-            field_values.append(value)
-        return data
+    def check_fields(self, data):
+        missing_fields = [field for field in self.fields if field not in data]
+        if missing_fields:
+            raise KeyError(
+                [f'Поле "{field}" отсутствует в сериализаторе'
+                 for field in missing_fields]
+            )
+
+    def __call__(self, data):
+        self.check_fields(data)
+        field_values = {data[field] for field in self.fields}
+        if len(field_values) != len(self.fields):
+            raise ValidationError(
+                [f'Значение "{data[field]}" поля "{field}" не уникально.'
+                 for field in self.fields]
+            )
